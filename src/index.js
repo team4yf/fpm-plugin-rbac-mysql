31065620412ab52dd5f41b71a43acde2a8fedf3a
yfsoftcom <yfsoftcom@126.com>
2018-05-03 17:07:16 +0800

    upgrade

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import _ from 'lodash'
 import { Factory } from './Rbac.js'
 export default {
   bind: (fpm) => {
-    const rbacFactory = Factory(fpm)
-    // Run When Server Init
-    fpm.registerAction('INIT', () => {
-      const c = fpm.getConfig()
-      console.log('Run Init Actions', c)
-    })
 
     fpm.registerAction('BEFORE_SERVER_START', () => {
+      const rbacFactory = Factory(fpm)
       fpm.rbacFactory = rbacFactory
-      console.log('Run BEFORE_SERVER_START Actions')
     })
 
   }
